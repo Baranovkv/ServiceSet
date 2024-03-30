b6cdd3fe3238ea269a1c36c19a18f7c741e8f1b7
Kirill <K-baranov@outlook.com>
2024-03-30 12:29:09 +0100

    Initial Commit

---
+++ ServiceSet/ServiceSetApp.swift
@@ -0,0 +1,17 @@
+//
+//  ServiceSetApp.swift
+//  ServiceSet
+//
+//  Created by Kirill Baranov on 30/03/24.
+//
+
+import SwiftUI
+
+@main
+struct ServiceSetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
